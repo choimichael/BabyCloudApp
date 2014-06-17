d6a2c0f708b70f9621de2dd96329f0c585dbfac2
Choi.Michael <typeinmichael@gmail.com>
2014-06-16 22:32:33 -0700

    Static Pages and Rspec running

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 BabyCloudApp::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'home#static_pages'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
