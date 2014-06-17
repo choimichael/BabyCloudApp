83d5083dffbc7a3b5f1dcde787021af6f14fdf8a
Choi.Michael <typeinmichael@gmail.com>
2014-06-16 21:13:59 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BabyCloudApp::Application.config.secret_key_base = '92429ba15096ecac64848e7812592faf5c57549bdd09185eb80bc43c53bccc43eeb3a5011c28e3be1052958133a7ff1ae63080cc287f2eeac70d81239b520ee4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BabyCloudApp::Application.config.session_store :cookie_store, key: '_baby_cloud_app_session'
