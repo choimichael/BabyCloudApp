3988ff2fc06a00194c8a9375fd23a3c3b6ff5653
cgdang <cgdang@gmail.com>
2014-06-17 20:42:23 -0700

    User avatar added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+	before_action :find_user, only: [:show, :edit, :update]
+
+	def new
+  	@user = User.new
+  end
+
+  def show
+    @posts = @user.posts.reverse
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:sucess] = "Signed Up Successfully"
+  		redirect_to :root
+  	else
+  		render :new
+  	end
+  end
+
+  protected
+
+  def user_params
+  	params.require(:user).permit(:email, :password, :avatar)
+  end
+
+  def find_user
+    @user = User.find_by(email: params[:id])
+  end
+
+end
+
