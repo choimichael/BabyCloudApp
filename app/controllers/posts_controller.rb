902c39dd6168165bc864aec5b7626306bfe2f17d
cgdang <cgdang@gmail.com>
2014-06-19 10:47:05 -0700

    CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  # before_action :authenticate_user
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /articles
+  def index
+    @posts = Post.all
+  end
+
+  def grid
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to posts_path, flash: 'Post was successfully created'
+    else
+      render :new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+    @post.update(post_params)
+    redirect_to @post, flash: 'Post was deleted'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description, :image)
+  end
+end
